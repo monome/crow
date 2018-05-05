3001e4961a37c0cc8431423bcbefb8b66e040abd
Trent <trent.gill@gmail.com>
2018-05-04 21:40:29 -0400

    update SPI defines for indepenedent DAC & ADC spis

--- lib/dac8565.c
+++ lib/dac8565.c
@@ -8,7 +8,7 @@ uint8_t aTxBuffer[64];
 void DAC_Init(void)
 {
     // Set the SPI parameters
-    dac_spi.Instance               = SPIx;
+    dac_spi.Instance               = SPId;
     dac_spi.Init.Mode              = SPI_MODE_MASTER;
     dac_spi.Init.Direction         = SPI_DIRECTION_1LINE;
     dac_spi.Init.DataSize          = SPI_DATASIZE_8BIT;
@@ -24,8 +24,8 @@ void DAC_Init(void)
     if(HAL_SPI_Init(&dac_spi) != HAL_OK){ Debug_USART_printf("spi_init\n\r"); }
 
     // NRST & NSS both high
-    HAL_GPIO_WritePin( SPIx_NSS_GPIO_PORT, SPIx_NSS_PIN, 1 );
-    HAL_GPIO_WritePin( SPIx_NRST_GPIO_PORT, SPIx_NRST_PIN, 1 );
+    HAL_GPIO_WritePin( SPId_NSS_GPIO_PORT, SPId_NSS_PIN, 1 );
+    HAL_GPIO_WritePin( SPId_NRST_GPIO_PORT, SPId_NRST_PIN, 1 );
 }
 
 void DAC_Set( int8_t channel, uint16_t value )
@@ -45,7 +45,7 @@ void DAC_Set( int8_t channel, uint16_t value )
     aTxBuffer[2] = (uint8_t)( value & 0xFF);
 
     // pull !SYNC low
-    HAL_GPIO_WritePin( SPIx_NSS_GPIO_PORT, SPIx_NSS_PIN, 0 );
+    HAL_GPIO_WritePin( SPId_NSS_GPIO_PORT, SPId_NSS_PIN, 0 );
     if(HAL_SPI_Transmit_DMA( &dac_spi
                            , (uint8_t*)aTxBuffer
                            , 3
@@ -59,14 +59,14 @@ void DAC_Set( int8_t channel, uint16_t value )
 void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi)
 {
     // signal end of transmission by pulling NSS high
-    HAL_GPIO_WritePin( SPIx_NSS_GPIO_PORT, SPIx_NSS_PIN, 1 );
+    HAL_GPIO_WritePin( SPId_NSS_GPIO_PORT, SPId_NSS_PIN, 1 );
 }
 
 void HAL_SPI_ErrorCallback(SPI_HandleTypeDef *hspi)
 {
     Debug_USART_printf("spi_error\n\r");
     // pull NSS high to cancel any ongoing transmission
-    HAL_GPIO_WritePin( SPIx_NSS_GPIO_PORT, SPIx_NSS_PIN, 1 );
+    HAL_GPIO_WritePin( SPId_NSS_GPIO_PORT, SPId_NSS_PIN, 1 );
 }
 
 void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
@@ -75,39 +75,39 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
 
     GPIO_InitTypeDef  GPIO_InitStruct;
 
-    SPIx_NSS_GPIO_CLK_ENABLE();
-    SPIx_SCK_GPIO_CLK_ENABLE();
-    SPIx_MOSI_GPIO_CLK_ENABLE();
-    SPIx_NRST_GPIO_CLK_ENABLE();
+    SPId_NSS_GPIO_CLK_ENABLE();
+    SPId_SCK_GPIO_CLK_ENABLE();
+    SPId_MOSI_GPIO_CLK_ENABLE();
+    SPId_NRST_GPIO_CLK_ENABLE();
 
-    SPIx_CLK_ENABLE();
+    SPId_CLK_ENABLE();
 
-    SPIx_DMAx_CLK_ENABLE();
+    SPId_DMAx_CLK_ENABLE();
 
     // GPIO pins
-    GPIO_InitStruct.Pin       = SPIx_SCK_PIN;
+    GPIO_InitStruct.Pin       = SPId_SCK_PIN;
     GPIO_InitStruct.Speed     = GPIO_SPEED_FAST;
     GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Alternate = SPIx_SCK_AF;
-    HAL_GPIO_Init(SPIx_SCK_GPIO_PORT, &GPIO_InitStruct);
+    GPIO_InitStruct.Alternate = SPId_SCK_AF;
+    HAL_GPIO_Init(SPId_SCK_GPIO_PORT, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin       = SPIx_MOSI_PIN;
-    GPIO_InitStruct.Alternate = SPIx_MOSI_AF;
-    HAL_GPIO_Init(SPIx_MOSI_GPIO_PORT, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin       = SPId_MOSI_PIN;
+    GPIO_InitStruct.Alternate = SPId_MOSI_AF;
+    HAL_GPIO_Init(SPId_MOSI_GPIO_PORT, &GPIO_InitStruct);
 
     // NSS & NRST handled manually
-    GPIO_InitStruct.Pin       = SPIx_NSS_PIN;
+    GPIO_InitStruct.Pin       = SPId_NSS_PIN;
     GPIO_InitStruct.Pull      = GPIO_PULLUP;
     GPIO_InitStruct.Mode      = GPIO_MODE_OUTPUT_PP;
-    HAL_GPIO_Init(SPIx_NSS_GPIO_PORT, &GPIO_InitStruct);
+    HAL_GPIO_Init(SPId_NSS_GPIO_PORT, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin       = SPIx_NRST_PIN;
-    HAL_GPIO_Init(SPIx_NRST_GPIO_PORT, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin       = SPId_NRST_PIN;
+    HAL_GPIO_Init(SPId_NRST_GPIO_PORT, &GPIO_InitStruct);
 
     // DMA Streams
-    hdma_tx.Instance                 = SPIx_TX_DMA_STREAM;
+    hdma_tx.Instance                 = SPId_TX_DMA_STREAM;
 
-    hdma_tx.Init.Channel             = SPIx_TX_DMA_CHANNEL;
+    hdma_tx.Init.Channel             = SPId_TX_DMA_CHANNEL;
     hdma_tx.Init.Direction           = DMA_MEMORY_TO_PERIPH;
     hdma_tx.Init.PeriphInc           = DMA_PINC_DISABLE;
     hdma_tx.Init.MemInc              = DMA_MINC_ENABLE;
@@ -126,12 +126,12 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
     __HAL_LINKDMA(hspi, hdmatx, hdma_tx);
 
     // DMA Priority (should be below IO, but above main process)
-    HAL_NVIC_SetPriority(SPIx_DMA_TX_IRQn, 0, 1);
-    HAL_NVIC_EnableIRQ(SPIx_DMA_TX_IRQn);
+    HAL_NVIC_SetPriority(SPId_DMA_TX_IRQn, 0, 1);
+    HAL_NVIC_EnableIRQ(SPId_DMA_TX_IRQn);
 
     // Must be lower priority than the above DMA
-    HAL_NVIC_SetPriority(SPIx_IRQn, 0, 2);
-    HAL_NVIC_EnableIRQ(SPIx_IRQn);
+    HAL_NVIC_SetPriority(SPId_IRQn, 0, 2);
+    HAL_NVIC_EnableIRQ(SPId_IRQn);
 }
 
 void HAL_SPI_MspDeInit(SPI_HandleTypeDef *hspi)
@@ -139,24 +139,24 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef *hspi)
 
   static DMA_HandleTypeDef hdma_tx;
 
-  SPIx_FORCE_RESET();
-  SPIx_RELEASE_RESET();
+  SPId_FORCE_RESET();
+  SPId_RELEASE_RESET();
 
-  HAL_GPIO_DeInit(SPIx_NSS_GPIO_PORT, SPIx_NSS_PIN);
-  HAL_GPIO_DeInit(SPIx_SCK_GPIO_PORT, SPIx_SCK_PIN);
-  HAL_GPIO_DeInit(SPIx_MOSI_GPIO_PORT, SPIx_MOSI_PIN);
+  HAL_GPIO_DeInit(SPId_NSS_GPIO_PORT, SPId_NSS_PIN);
+  HAL_GPIO_DeInit(SPId_SCK_GPIO_PORT, SPId_SCK_PIN);
+  HAL_GPIO_DeInit(SPId_MOSI_GPIO_PORT, SPId_MOSI_PIN);
 
   HAL_DMA_DeInit(&hdma_tx);
 
-  HAL_NVIC_DisableIRQ(SPIx_DMA_TX_IRQn);
-  HAL_NVIC_DisableIRQ(SPIx_IRQn);
+  HAL_NVIC_DisableIRQ(SPId_DMA_TX_IRQn);
+  HAL_NVIC_DisableIRQ(SPId_IRQn);
 }
 
-void SPIx_DMA_TX_IRQHandler(void)
+void SPId_DMA_TX_IRQHandler(void)
 {
     HAL_DMA_IRQHandler(dac_spi.hdmatx);
 }
-void SPIx_IRQHandler(void)
+void SPId_IRQHandler(void)
 {
     HAL_SPI_IRQHandler(&dac_spi);
 }
--- lib/dac8565.h
+++ lib/dac8565.h
@@ -4,41 +4,41 @@
 
 #include "stm32f4xx_hal_conf.h"
 
-// Definition for SPIx clock resources
-#define SPIx                             SPI2
-#define SPIx_CLK_ENABLE()                __HAL_RCC_SPI2_CLK_ENABLE()
-#define SPIx_DMAx_CLK_ENABLE()           __HAL_RCC_DMA1_CLK_ENABLE()
-#define SPIx_NSS_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOB_CLK_ENABLE()
-#define SPIx_SCK_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOB_CLK_ENABLE()
-#define SPIx_MOSI_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOB_CLK_ENABLE()
-#define SPIx_NRST_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOB_CLK_ENABLE()
+// Definition for SPId clock resources
+#define SPId                             SPI2
+#define SPId_CLK_ENABLE()                __HAL_RCC_SPI2_CLK_ENABLE()
+#define SPId_DMAx_CLK_ENABLE()           __HAL_RCC_DMA1_CLK_ENABLE()
+#define SPId_NSS_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOB_CLK_ENABLE()
+#define SPId_SCK_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOB_CLK_ENABLE()
+#define SPId_MOSI_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOB_CLK_ENABLE()
+#define SPId_NRST_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOB_CLK_ENABLE()
 
-#define SPIx_FORCE_RESET()               __HAL_RCC_SPI2_FORCE_RESET()
-#define SPIx_RELEASE_RESET()             __HAL_RCC_SPI2_RELEASE_RESET()
+#define SPId_FORCE_RESET()               __HAL_RCC_SPI2_FORCE_RESET()
+#define SPId_RELEASE_RESET()             __HAL_RCC_SPI2_RELEASE_RESET()
 
-// Definition for SPIx Pins
-#define SPIx_NSS_PIN                     GPIO_PIN_12
-#define SPIx_NSS_GPIO_PORT               GPIOB
-#define SPIx_SCK_PIN                     GPIO_PIN_13
-#define SPIx_SCK_GPIO_PORT               GPIOB
-#define SPIx_SCK_AF                      GPIO_AF5_SPI2
-#define SPIx_MOSI_PIN                    GPIO_PIN_15
-#define SPIx_MOSI_GPIO_PORT              GPIOB
-#define SPIx_MOSI_AF                     GPIO_AF5_SPI2
-#define SPIx_NRST_PIN                    GPIO_PIN_14
-#define SPIx_NRST_GPIO_PORT              GPIOB
+// Definition for SPId Pins
+#define SPId_NSS_PIN                     GPIO_PIN_12
+#define SPId_NSS_GPIO_PORT               GPIOB
+#define SPId_SCK_PIN                     GPIO_PIN_13
+#define SPId_SCK_GPIO_PORT               GPIOB
+#define SPId_SCK_AF                      GPIO_AF5_SPI2
+#define SPId_MOSI_PIN                    GPIO_PIN_15
+#define SPId_MOSI_GPIO_PORT              GPIOB
+#define SPId_MOSI_AF                     GPIO_AF5_SPI2
+#define SPId_NRST_PIN                    GPIO_PIN_14
+#define SPId_NRST_GPIO_PORT              GPIOB
 
-// Definition for SPIx's DMA
-#define SPIx_TX_DMA_CHANNEL              DMA_CHANNEL_0
-#define SPIx_TX_DMA_STREAM               DMA1_Stream4
+// Definition for SPId's DMA
+#define SPId_TX_DMA_CHANNEL              DMA_CHANNEL_0
+#define SPId_TX_DMA_STREAM               DMA1_Stream4
 
-// Definition for SPIx's NVIC
-#define SPIx_IRQn                        SPI2_IRQn
-#define SPIx_IRQHandler                  SPI2_IRQHandler
-#define SPIx_DMA_TX_IRQn                 DMA1_Stream4_IRQn
-#define SPIx_DMA_RX_IRQn                 DMA1_Stream3_IRQn
-#define SPIx_DMA_TX_IRQHandler           DMA1_Stream4_IRQHandler
-#define SPIx_DMA_RX_IRQHandler           DMA1_Stream3_IRQHandler
+// Definition for SPId's NVIC
+#define SPId_IRQn                        SPI2_IRQn
+#define SPId_IRQHandler                  SPI2_IRQHandler
+#define SPId_DMA_TX_IRQn                 DMA1_Stream4_IRQn
+#define SPId_DMA_RX_IRQn                 DMA1_Stream3_IRQn
+#define SPId_DMA_TX_IRQHandler           DMA1_Stream4_IRQHandler
+#define SPId_DMA_RX_IRQHandler           DMA1_Stream3_IRQHandler
 
 // dac8565 defines
 #define DAC8565_SET_ONE     0b00010000
@@ -53,6 +53,6 @@ void DAC_Init(void);
 
 void DAC_Set( int8_t channel, uint16_t value );
 
-void SPIx_DMA_RX_IRQHandler(void);
-void SPIx_DMA_TX_IRQHandler(void);
-void SPIx_IRQHandler(void);
+void SPId_DMA_RX_IRQHandler(void);
+void SPId_DMA_TX_IRQHandler(void);
+void SPId_IRQHandler(void);
