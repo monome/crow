fe405184a710d39110af06da39bcb002aeeebdd6
Trent <trent.gill@gmail.com>
2018-09-23 21:51:36 -0400

    absolute lua paths

--- lib/lualink.c
+++ lib/lualink.c
@@ -3,9 +3,9 @@
 #include <string.h> // strcmp()
 
 // Lua itself
-#include <lua.h>
-#include <lauxlib.h>
-#include <lualib.h>
+#include "../../lua/src/lua.h"
+#include "../../lua/src/lauxlib.h"
+#include "../../lua/src/lualib.h"
 
 // Hardware IO
 #include "ll/debug_usart.h" // U_Print*()
