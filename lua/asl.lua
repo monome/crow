1303ca45479d66890c6565d43d77ebd146eedc37
Trent <trent.gill@gmail.com>
2018-10-20 19:29:52 -0400

    rewrite asl using coroutines instead of asm style

--- lua/asl.lua
+++ lua/asl.lua
@@ -2,6 +2,9 @@
 -- declarative scripts for time-based event structures
 -- scripts can have compile-time, or call-time behaviour and
 -- query hardware states at the appropriate time
+--
+--
+-- rewrite using coroutine approach for the execution of the ASL
 
 local table = require('table')
 local print = print
@@ -19,9 +22,7 @@ function Asl.new(id)
 end
 
 function Asl:init() -- reset to defaults
-    self.program = {}     -- store the ASL program
-    self.retStk  = {}     -- return stack for nested constructs
-    self.pc      = 0      -- program counter within stack frame
+    self.co      = nil    -- a coroutine to be!
     self.hold    = false  -- is the slope trigger currently held high
     self.in_hold = false  -- is eval currently in a held construct
     self.locked  = false  -- flag to lockout bangs during lock{}
@@ -30,31 +31,13 @@ end
 -- COMPILER
 -- create a table with a sequence of functions
 
-local function exit(self)
-    self.pc = self.retStk[#self.retStk]
-    table.remove( self.retStk )
-end
-
--- deferred versions of intrinsics
-local function q_exit() return exit end
-
-local function compileT( aslT )
-    local x = {}
-    for k, fn in ipairs(aslT) do
-        table.insert( x, fn )
-    end
-    table.insert( x, q_exit() )
-    return x
-end
-
 -- use a metamethod so we can can *assign* myAsl:action = lfo()
 -- but then *call* myAsl:action(high/low) 
 -- need a 'proxy' metatable. see: https://www.lua.org/pil/13.4.4.html
-function Asl:action( aslT )
-    self.program  = compileT( aslT )
-    self.retStk   = {}
-    self.hold     = false
-    self.locked   = false
+function Asl:action( fn )
+    self.co     = fn
+    self.hold   = false
+    self.locked = false
 end
 
 
@@ -65,135 +48,87 @@ end
 -- this is stupid. we're searching the tree every step!
 -- think about how to do this in a direct way
 
--- returns the current table context of the program counter
---> helper step()
-local function get_frame(self)
-    local frame = self.program -- the table representing the program
-    for i=1, #self.retStk, 1 do
-        frame = frame[self.retStk[i]]
-    end
-    return frame
-end
-
 function Asl:step()
-    local p = get_frame(self)                      -- find table level
-    if p[self.pc] == nil then print('asl.pc == nil') return
-    elseif type(p[self.pc]) == ('function') then
-        wait = p[self.pc](self)                    -- execute the step
-        if self.pc ~= nil then
-            self.pc = self.pc + 1
-            if wait ~= 'wait' then self:step() end -- recurse for next step
-        end -- else we did last exit()
-    else
-        table.insert( self.retStk, self.pc )
-        self.pc = 1
-        self:step()                                -- unpack table & recurse
-    end
+    _,wait = coroutine.resume(self.co, self)
+    if wait ~= 'wait' then self:step() end
 end
 
 function Asl:set_hold_state(state)
     self.hold = state
 end
 
-local function start( self )
-    if self.program ~= nil then
-        self.pc      = 1
-        self.retStk  = {}
-    end
-    self:step()
-end
-
-local function release( self )
-    self.hold = false
-    if self.in_hold then
-        self.in_hold = false
-        exit(self)
-    end
-end
-
 -- public accessors
 function Asl:bang( dir )
     self.hold = dir
-    if self.program ~= nil then
+    if self.co ~= nil then
         if self.locked ~= true then
-            if dir == true then
-                start( self )
-            else
-                release( self )
-            end
+            --TODO release handling
+            self:step()
         end
     end
 end
-
-
-
-
 
 -- hw access fn
 function toward( dest, time, shape )
     local d,t,s = dest or 0, time or 1, shape or 'linear'
-    return function(self)
-        LL_toward(self,d,t,s)
-        if t ~= 0 then
-            return 'wait'
+    return coroutine.create(function(self)
+        while true do
+            LL_toward(self,d,t,s)
+            coroutine.yield( (t ~= 0) and 'wait' or nil )
         end
-    end
+    end)
 end
 
 -- sugared toward(). could be variable arity instead?
 function now( dest )
-    return toward(d,0,LINEAR)
+    return toward( dest, 0, LINEAR )
 end
 
 function delay( time )
+    --TODO here needs to call self to find current state
     local here = 0 -- can this be a metamethod call?
-    return toward( here, t, LINEAR )
+    return toward( here, time, LINEAR )
 end
-
-
-
-
 
 -- WRAPPING functions
-function asl_if( fn_to_bool, aslT )
-    local ftb = fn_to_bool
-    t = compileT( aslT )
-    table.insert( t
-                , 1
-                , function(self)
-                    if ftb(self) == false then exit(self) end
-                  end
-                )
-    return t
+function asl_if( fn_to_bool, fns )
+    return coroutine.create(function(self)
+        if fn_to_bool(self) then
+            for i=1, #fns, 1 do
+                _,wait = coroutine.resume(fns[i], self)
+                coroutine.yield(wait)
+            end
+        end
+    end)
 end
 
--- replace pc = 0 with tag(tagname) goto(tagname) pairs
-function q_recur()
-    return function(self) self.pc = 0 end
+function asl_wrap( enter_fn, fns, exit_fn )
+    return coroutine.create(function(self)
+        enter_fn(self)
+        for i=1, #fns, 1 do
+            _,wait = coroutine.resume(fns[i], self)
+            coroutine.yield(wait)
+        end
+        exit_fn(self)
+    end)
 end
-
-function asl_wrap( enter_fn, aslProgram, exit_fn )
-    p = aslProgram
-    table.insert( p,  1, enter_fn )
-    table.insert( p, #p, exit_fn )
-    return p
-end
-
-
-
-
 
 -- first layer of abstraction over VM
-function loop( aslT )
-    t = compileT( aslT )
-    t[#t] = q_recur() -- overwrite exit() with recur()
-    return t
+function loop( fns )
+    return coroutine.create(function(self)
+        while true do
+            for i=1,#fns,1 do
+                _,wait = coroutine.resume(fns[i], self)
+                coroutine.yield(wait)
+            end
+        end
+    end)
 end
 
 -- could abstract this form into asl_wrap()
-function lock( aslT )
+function lock( fns )
     return asl_wrap( function(self) self.lock = true end
-                   , compileT( aslT )
+                   , fns
                    , function(self) self.lock = false end
                    )
 end
@@ -208,14 +143,11 @@ function held( aslT )
 end
 
 function times( count, aslT )
-    local n = count -- does this become an issue in the class? asl.n?
-    t = asl_if( function(self) local result = n > 0
-                               n = n - 1
-                               return result end
-              , aslT
-              )
-    t[#t] = q_recur()
-    return t
+    local n = count
+    return asl_if( function(self) n = n - 1
+                                  return (n > -1) end
+                 , aslT
+                 )
 end
 
 print 'asl lib loaded'
--- ref/test.lua
+++ ref/test.lua
@@ -1,3 +1,8 @@
+-- dummy functions
+function go_toward( id,d,t,s ) end
+
+
+
 -- must be provided for asl.lua
 function LL_toward( self, d, t, s )
     -- that would allow one to access the state of the output in a closure
@@ -32,88 +37,17 @@ local function lfo( speed, curve, level )
     curve = curve or "linear"
     level = level or 5
 
-    return { loop{ toward( q_random(-5,5), speed, curve )
-                 , toward( -level, speed, curve )
-                 }
-           }
-end
+    return loop{ toward( q_random(-5,5), speed, curve )
+               , toward( -level, speed, curve )
+               }
 
-local function tester2(sl)
-    sl:action( lfo() )
-end
-
-local function tester3()
-    sl:action
-        { now(3)
-        , asl_if( function() return true end
-                , { toward( -3 )
-                  , toward( 4.5 )
-                  }
-                )
-        , toward(4)
-        }
-end
-
-local function tester4(sl)
-    sl:action
-        { toward(7)
-        , times( 2, { toward(3)
-                    , toward(-4)
-                    }
-               )
-        , toward(-5)
-        }
-end
-
-local function tester5(sl)
-    sl:action
-        { toward(5)
-        , held{ toward(4)
-              , toward(3)
-              , toward(2)
-              , toward(1)
-              , toward(0)
-              }
-        , toward(-1)
-        , toward(-2)
-        , toward(-3)
-        , toward(-4)
-        }
 end
 
 local sl = {}
 sl[1] = Asl.new(1)
---sl[2] = Asl.new(2)
---tester5(sl[1])
---sl[2]:action(lfo())
 sl[1]:action(lfo())
 sl[1]:bang(true)
---set_hold_state(false)
-sl[1]:callback()
---sl[2]:bang(true)
-sl[1]:callback()
-sl[1]:callback()
-sl[1]:callback()
---sl[1]:bang(false)
---sl[2]:callback()
---sl[2]:callback()
---sl[1]:callback()
---sl[1]:callback()
---sl[2]:callback()
---sl[1]:callback()
---sl[2]:callback()
---sl[1]:callback()
---asl:bang(false)
-
--- desired representation of the above asl:action
--- { now(0)
--- , { toward( 5, 2, "sine")
---   , toward( -5, 2, "sine")
---   , restart_frame()
---   }
--- , exit(0)
--- }
---
---
---caller of exit
-
+sl[1]:step()
+sl[1]:step()
+sl[1]:step()
+sl[1]:step()
