cacc266200196ff5be8e6e10d0dd35587cb5cc98
trentgill <trent.gill@gmail.com>
2018-11-06 21:33:56 -0500

    cleanup comments and formatting

--- lua/asl.lua
+++ lua/asl.lua
@@ -2,20 +2,14 @@
 -- declarative scripts for time-based event structures
 -- scripts can have compile-time, or call-time behaviour and
 -- query hardware states at the appropriate time
---
---
--- rewrite using coroutine approach for the execution of the ASL
-
-local table = require('table')
-local print = print
 
 local Asl = {}
 Asl.__index = Asl
 
 function Asl.new(id)
     local slope = {}
-    setmetatable(slope, Asl)
-    if id == nil then print('asl needs id of output channel') end
+    setmetatable( slope, Asl )
+    if id == nil then print'asl needs id of output channel' end
     slope.id = id or 1
     slope:init()
     return slope
@@ -29,7 +23,6 @@ function Asl:init() -- reset to defaults
 end
 
 -- COMPILER
--- create a table with a sequence of functions
 
 -- use a metamethod so we can can *assign* myAsl:action = lfo()
 -- but then *call* myAsl:action(high/low) 
@@ -42,22 +35,16 @@ end
 
 
 -- RUNTIME behaviour
--- relates bang() to internal AST representation
-
--- step through the program recursively!
--- this is stupid. we're searching the tree every step!
--- think about how to do this in a direct way
 
 function Asl:step()
-    _,wait = coroutine.resume(self.co, self)
+    _,wait = coroutine.resume( self.co, self )
     if wait ~= 'wait' then self:step() end
 end
 
-function Asl:set_hold_state(state)
+function Asl:set_hold_state( state )
     self.hold = state
 end
 
--- public accessors
 function Asl:bang( dir )
     self.hold = dir
     if self.co ~= nil then
@@ -69,81 +56,81 @@ function Asl:bang( dir )
 end
 
 -- hw access fn
+-- TODO should this take args in a table for curly braces
+-- there's an inferred inference in how it's used
+-- but it's not just a wrapped 'go_toward' as it abstracts
+-- the id based on the asl in use
 function toward( dest, time, shape )
-    local d,t,s = dest or 0, time or 1, shape or 'linear'
-    return coroutine.create(function(self)
+    local d,t,s
+    if type(dest) == 'table' then -- accept table syntax
+        local tt = dest
+        d,t,s = tt.dest or 'here', tt.time or 0, tt.shape or 'linear'
+    else
+        d,t,s = dest or 'here', time or 0, shape or 'linear'
+    end
+    return coroutine.create(function( self )
         while true do
-            LL_toward(self,d,t,s)
+            if d == 'here' then d = 1 end -- TODO d=1 should get 'here'
+            LL_toward( self.id, d, t, s )
             coroutine.yield( (t ~= 0) and 'wait' or nil )
         end
     end)
 end
 
--- sugared toward(). could be variable arity instead?
-function now( dest )
-    return toward( dest, 0, LINEAR )
-end
-
-function delay( time )
-    --TODO here needs to call self to find current state
-    local here = 0 -- can this be a metamethod call?
-    return toward( here, time, LINEAR )
-end
 
 -- WRAPPING functions
 function seq_coroutines( self, fns )
     for i=1, #fns, 1 do
-        local _,wait = coroutine.resume(fns[i], self)
-        coroutine.yield(wait)
+        local _,wait = coroutine.resume( fns[i], self )
+        coroutine.yield( wait )
     end
 end
 
 function asl_if( fn_to_bool, fns )
-    return coroutine.create(function(self)
-        if fn_to_bool(self) then
-            seq_coroutines(self, fns)
+    return coroutine.create(function( self )
+        if fn_to_bool( self ) then
+            seq_coroutines( self, fns )
         end
     end)
 end
 
 function asl_wrap( enter_fn, fns, exit_fn )
-    return coroutine.create(function(self)
-        enter_fn(self)
-        seq_coroutines(self, fns)
-        exit_fn(self)
+    return coroutine.create(function( self )
+        enter_fn( self )
+        seq_coroutines( self, fns )
+        exit_fn( self )
     end)
 end
 
 -- first layer of abstraction over VM
 function loop( fns )
-    return coroutine.create(function(self)
+    return coroutine.create(function( self )
         while true do
-            seq_coroutines(self, fns)
+            seq_coroutines( self, fns )
         end
     end)
 end
 
--- could abstract this form into asl_wrap()
 function lock( fns )
-    return asl_wrap( function(self) self.lock = true end
+    return asl_wrap( function( self ) self.lock = true end
                    , fns
-                   , function(self) self.lock = false end
+                   , function( self ) self.lock = false end
                    )
 end
 
 function held( aslT )
-    return asl_wrap( function(self) self.in_hold = true end
-                   , asl_if( function(self) return self.hold end
+    return asl_wrap( function( self ) self.in_hold = true end
+                   , asl_if( function( self ) return self.hold end
                            , aslT
                            )
-                   , function(self) self.in_hold = false end
+                   , function( self ) self.in_hold = false end
                    )
 end
 
 function times( count, aslT )
     local n = count
-    return asl_if( function(self) n = n - 1
-                                  return (n > -1) end
+    return asl_if( function( self ) n = n - 1
+                                    return (n > -1) end
                  , aslT
                  )
 end
