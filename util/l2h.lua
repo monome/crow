840370869d33362c8666db63e36e5638f6959191
Trent <trent.gill@gmail.com>
2018-11-30 23:33:26 -0500

    update l2h after fennel overhaul

--- util/l2h.lua
+++ util/l2h.lua
@@ -1,7 +1,7 @@
 local function _0_(src, dst)
   local nextline = src.read(src, "l")
   if (nextline ~= nil) then
-    dst.write(dst, (("\"" .. nextline) .. "\\n\"\n\9"))
+    dst.write(dst, ("\"" .. nextline .. "\\n\"\n\t"))
     return writeline(src, dst)
   end
 end
@@ -10,7 +10,7 @@ do
   local filename = arg[1]
   do
     local f = io.open((filename .. ".h"), "w")
-    f.write(f, (("#pragma once\n\nchar " .. string.gsub(string.sub(filename, 1, -5), "/", "_")) .. "[]="))
+    f.write(f, ("#pragma once\n\nchar " .. string.gsub(string.sub(filename, 1, -5), "/", "_") .. "[]="))
     writeline(assert(io.open(filename, "r")), f)
     f.write(f, ";")
     return f.close(f)
