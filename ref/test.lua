ebc873b57999c87c1f7c283a24c66d9f0b6ca664
Trent <trent.gill@gmail.com>
2018-11-30 22:00:20 -0500

    update test to reflect changes in lua libs

--- ref/test.lua
+++ ref/test.lua
@@ -1,29 +1,16 @@
 -- dummy functions
 function go_toward( id,d,t,s ) end
 
-
-
 -- must be provided for asl.lua
-function LL_toward( self, d, t, s )
+function LL_toward( id, d, t, s )
     -- that would allow one to access the state of the output in a closure
     if type(d) == 'function' then d = d() end
     if type(t) == 'function' then t = t() end
-    print("id: "..self.id,"\ttoward "..d,"\tin time: "..t,"\twith shape: "..s)
-    go_toward( self.id, d, t, s )
+    print("id: "..id,"\ttoward "..d,"\tin time: "..t,"\twith shape: "..s)
+    go_toward( id, d, t, s )
 end
 
-
-local Asl = dofile("lua/asl.lua")
-
-local function tester(sl)
-    local shape = "sine"
-    sl:action
-        { now(0)                            -- reset LFO to 0
-        , loop{ toward(  5, 2, shape ) -- LFO rises
-              , toward( -5, 2, shape ) -- LFO falls
-              }                             -- repeats rise&fall
-        }
-end
+Asl = dofile("lua/asl.lua")
 
 -- closured fns to be invoked fresh each time
 function q_random( lower, upper )
@@ -31,7 +18,7 @@ function q_random( lower, upper )
 end
 
 -- example of a 'library' shape
-local function lfo( speed, curve, level )
+function lfo( speed, curve, level )
     -- default values for variadic fn use
     speed = speed or 1
     curve = curve or "linear"
@@ -40,10 +27,9 @@ local function lfo( speed, curve, level )
     return loop{ toward( q_random(-5,5), speed, curve )
                , toward( -level, speed, curve )
                }
-
 end
 
-local sl = {}
+sl = {1,2,3,4}
 sl[1] = Asl.new(1)
 sl[1]:action(lfo())
 sl[1]:bang(true)
