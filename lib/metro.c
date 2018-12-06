dc3932d9c8432fc85fa5c90eca350f37e0f48faf
Trent <trent.gill@gmail.com>
2018-12-06 16:27:37 -0500

    refine metros. add comments for cruft cleanup later

--- lib/metro.c
+++ lib/metro.c
@@ -13,9 +13,9 @@ enum {
 };
 
 typedef struct{
-    int      ix;       // metro index
+    int      ix;       // TODO never used. metro index
     int      status;   // running/stopped status
-    float    seconds;  // TODO never actually referred! period in seconds
+    float    seconds;  // TODO never used. period in seconds
     int32_t  count;    // number of repeats. <0 is infinite
     int32_t  stage;    // number of repeateds.
 } Metro_t;
@@ -33,6 +33,7 @@ void Metro_Init(void)
     metros = malloc( sizeof(Metro_t) * max_num_metros );
 
     for( int i=0; i<max_num_metros; i++ ){
+        metros[i].ix      = i;
         metros[i].status  = METRO_STATUS_STOPPED;
         metros[i].seconds = 1.0;
         metros[i].count   = -1;
@@ -86,7 +87,7 @@ static void metro_bang( int ix )
     L_handle_metro( ix, metros[ix].stage );
     metros[ix].stage++;
 //FIXME next line causes system not to load?
-    //if( metros[ix].stage < -1 ){ metros[ix].stage = 0x7FFF; } // overflow
+    if( metros[ix].stage == 0x7FFFFFFF ){ metros[ix].stage = 0x7FFFFFFE; } // overflow
     if( metros[ix].count >= 0 ){ // negative values are infinite
         if( metros[ix].stage > metros[ix].count ){
             metro_stop(ix);
