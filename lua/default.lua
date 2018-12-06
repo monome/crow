f6d282261705442bcb34f85b53860f7cfa02fce9
Trent <trent.gill@gmail.com>
2018-12-06 16:29:02 -0500

    add testing for counter lua metro lib to default projct

--- lua/default.lua
+++ lua/default.lua
@@ -3,6 +3,14 @@ function init()
     for c=1, 4 do
         out[c].asl:bang(true)
     end
+
+
+    counter = Metro.alloc()
+    counter.time = 1
+    counter.count = -1
+    counter.callback = count
+    counter:start()
+
 --    go_toward( 1, 5, 10, 'linear' )
 --
 --    go_toward{ channel  = 1
@@ -17,4 +25,10 @@ function init()
 --    -- ideally (with metatables)
 --    out[1].action = lfo()   -- set action to be lfo (must clear 'locked')
 --    out[1]:action(true)     -- do action with 'high' state (ie. reset)
+end
+
+position = 0
+function count(c)
+    position = position + 1
+    print(c .. '> ' .. position)
 end
