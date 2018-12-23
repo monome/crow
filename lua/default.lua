ebdb06162b70ee3721205a73d6b362e2fbe06d4e
Trent <trent.gill@gmail.com>
2018-12-22 21:59:43 -0500

    syntax update for asl

--- lua/default.lua
+++ lua/default.lua
@@ -1,7 +1,7 @@
 function init()
     -- start all the lfos
     for c=1, 4 do
-        out[c].asl:bang(true)
+        out[c].asl:action()
     end
 
 
@@ -10,21 +10,6 @@ function init()
     counter.count = -1
     counter.callback = count
     counter:start()
-
---    go_toward( 1, 5, 10, 'linear' )
---
---    go_toward{ channel  = 1
---             , level    = 5
---             , time     = 10
---             , slope    = 'linear'
---             }
-
---    slope[1]:action( lfo() )
---    slope[1]:bang(true)
---
---    -- ideally (with metatables)
---    out[1].action = lfo()   -- set action to be lfo (must clear 'locked')
---    out[1]:action(true)     -- do action with 'high' state (ie. reset)
 end
 
 position = 0
