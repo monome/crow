2bf666578067579172dc4cf239416c1c4fb79e5e
Trent <trent.gill@gmail.com>
2019-01-06 15:53:01 -0500

    MPU activation

--- main.c
+++ main.c
@@ -16,9 +16,13 @@
 
 static void Sys_Clk_Config(void);
 static void Error_Handler(void);
+static void MPU_Config(void);
+static void CPU_CACHE_Enable(void);
 
 int main(void)
 {
+	MPU_Config();
+	CPU_CACHE_Enable();
     HAL_Init();
     Sys_Clk_Config();
 
@@ -93,6 +97,42 @@ static void Sys_Clk_Config(void)
         Error_Handler();
     }
 }
+
+static void MPU_Config(void)
+{
+	MPU_Region_InitTypeDef MPU_InitStruct;
+
+	// Disable the MPU
+	HAL_MPU_Disable();
+
+	// Configure the MPU attributes as WT for SRAM
+	MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+	MPU_InitStruct.BaseAddress = 0x20020000;
+	MPU_InitStruct.Size = MPU_REGION_SIZE_512KB;
+	MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+	MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+	MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+	MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+	MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+	MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+	MPU_InitStruct.SubRegionDisable = 0x00;
+	MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+
+	HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+	// Enable the MPU
+	HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+}
+
+static void CPU_CACHE_Enable(void)
+{
+	// Enable I-Cache
+	SCB_EnableICache();
+
+	// Enable D-Cache
+	SCB_EnableDCache();
+}
+
 
 static void Error_Handler(void)
 {
