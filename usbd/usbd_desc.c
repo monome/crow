b636a4d1f71bcbf2590cce95b9db27005c2dad91
Trent <trent.gill@gmail.com>
2019-01-23 12:10:26 -0500

    update names of usb cdc descriptor

--- usbd/usbd_desc.c
+++ usbd/usbd_desc.c
@@ -55,9 +55,9 @@
 #define USBD_VID                      0x0483
 #define USBD_PID                      0x5740
 #define USBD_LANGID_STRING            0x409
-#define USBD_MANUFACTURER_STRING      "STMicroelectronics"
+#define USBD_MANUFACTURER_STRING      "monome & whimsical raps"
 #define USBD_PRODUCT_HS_STRING        "STM32 Virtual ComPort in HS Mode"
-#define USBD_PRODUCT_FS_STRING        "STM32 Virtual ComPort in FS Mode"
+#define USBD_PRODUCT_FS_STRING        "crow: telephone line"
 #define USBD_CONFIGURATION_HS_STRING  "VCP Config"
 #define USBD_INTERFACE_HS_STRING      "VCP Interface"
 #define USBD_CONFIGURATION_FS_STRING  "VCP Config"
