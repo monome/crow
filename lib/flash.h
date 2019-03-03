185eb053e07ef472c1b7f89b969b92a9de898dff
Trent <trent.gill@gmail.com>
2019-03-03 17:59:41 -0500

    reduced userscript size to 4k due to issue where malloc would fail out of mem

--- lib/flash.h
+++ lib/flash.h
@@ -10,7 +10,8 @@
 // 64kB user script
 #define USER_SCRIPT_LOCATION 0x08010000
 #define USER_SCRIPT_SECTOR   FLASH_SECTOR_4
-#define USER_SCRIPT_SIZE     (0x10000 - 4)
+//#define USER_SCRIPT_SIZE     (0x10000 - 4)
+#define USER_SCRIPT_SIZE     (0x1000 - 4)
 
 typedef enum { FLASH_Status_Init  = 0
              , FLASH_Status_Saved = 1
