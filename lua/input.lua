monome/crow

commit d078ce910a8a5f790fa210139e794479cec09fc6

change_handler now passes state as a bool (#149)

lua/input.lua
@@ -107,7 +107,7 @@ setmetatable(Input, Input) -- capture the metamethods
 
 -- callback
 function stream_handler( chan, val ) Input.inputs[chan].stream( val ) end
-function change_handler( chan, val ) Input.inputs[chan].change( val ) end
+function change_handler( chan, val ) Input.inputs[chan].change( val ~= 0 ) end
 function midi_handler( ... ) d = {...}; Input.inputs[1].midi(d) end
 
 print 'input loaded'

commit a85e329dadfa2f847d0c73d20094d98d267b3066

Fix bug caused by #149 in input[n].change default function (#151)
* change_handler now passes state as a bool

* change boolean is sent as a 1/0 for c.tell

lua/input.lua
@@ -20,7 +20,7 @@ function Input.new( chan )
               , ratios     = {}
         -- user-customizable events
               , stream     = function(value) _c.tell('stream',chan,value) end
-              , change     = function(state) _c.tell('change',chan,state) end
+              , change     = function(state) _c.tell('change',chan,state and 1 or 0) end
               , midi       = function(data) _c.tell('midi',table.unpack(data)) end
               , window     = function(ix, direction) get_cv(chan) end
               , scale      = function(octave, ix) get_cv(chan) end