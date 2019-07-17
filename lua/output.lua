6360a6b9581cf9dffef3c6c4554647e51c121fbe
Trent <trent.gill@gmail.com>
2019-07-17 11:36:21 -0400

    correct behaviour when calling the output table directly with an argument

--- lua/output.lua
+++ lua/output.lua
@@ -42,20 +42,7 @@ Output.__index = function(self, ix)
 end
 
 Output.__call = function(self, ...)
-    local args = {...}
-    if #args == 0 then
-        self.asl:action()
-    else -- table call
-        self.asl.action = args[1]
-        self.asl:action()
-        --local m = 0
-        ----if #args[1] == 0 then _ end -- implies empty table call
-        --for k,v in pairs( args[1] ) do
-        --    if k == 'mode' then m = v end -- defer mode change after setting params
-        --    self[k] = v
-        --end
-        --if m ~= 0 then self.mode = m end -- apply mode change
-    end
+    self.asl:action(...)
 end
 
 setmetatable(Output, Output) -- capture the metamethods
