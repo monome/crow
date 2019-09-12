8cbe95194a3f51841107b03724363bf562714267
brian crabtree <tehn@monome.org>
2019-09-12 11:35:37 -0400

    ii add (#166)
    * kria start
    
    * tt mode
    
    * fix ansible
    
    * typo, ansible now works
    
    * add ansible_mp
    
    * update subs
    
    * update for s16V
    
    * fix submodule

---
+++ lua/ii/ansible_kria.lua
@@ -0,0 +1,96 @@
+do return
+{ module_name  = 'ansible_kria'
+, manufacturer = 'monome'
+, i2c_address  = 0x28
+, lua_name     = 'ansible_kria'
+, commands     =
+  { { name = 'preset'
+    , cmd  = 0
+    , get  = true
+    , docs = 'set preset'
+    , args = { 'number', u8 }
+    }
+  , { name = 'pattern'
+    , cmd  = 1
+    , get  = true
+    , docs = 'set pattern'
+    , args = { 'number', u8 }
+    }
+  , { name = 'scale'
+    , cmd  = 2
+    , get  = true
+    , docs = 'set scale'
+    , args = { 'number', u8 }
+    }
+  , { name = 'period'
+    , cmd  = 3
+    , get  = true
+    , docs = 'set internal clock period'
+    , args = { 'time', u16 }
+    }
+  , { name = 'position'
+    , cmd  = 4
+    , get  = true
+    , docs = 'set position'
+    , args = { { 'track', u8 }
+             , { 'param', u8 }
+             , { 'pos', u8 }
+           }
+    }
+  , { name = 'loop_start'
+    , cmd  = 5
+    , get  = true
+    , docs = 'set loop start'
+    , args = { { 'track', u8 }
+             , { 'param', u8 }
+             , { 'pos', u8 }
+           }
+    }
+  , { name = 'loop_length'
+    , cmd  = 6
+    , get  = true
+    , docs = 'set loop length'
+    , args = { { 'track', u8 }
+             , { 'param', u8 }
+             , { 'pos', u8 }
+           }
+    }
+  , { name = 'reset'
+    , cmd  = 7
+    , get  = true
+    , docs = 'reset position to start'
+    , args = { { 'track', u8 }
+             , { 'param', u8 }
+           }
+    }
+  -- CMD 8, cv, only has getter??
+  , { name = 'mute'
+    , cmd  = 9
+    , get  = true
+    , docs = 'set mute state'
+    , args = { { 'channel', u8 }
+             , { 'state', u8 }
+           }
+    }
+  , { name = 'toggle_mute'
+    , cmd  = 10
+    , get  = false
+    , docs = 'toggle mute state'
+    , args = { 'channel', u8 }
+    }
+  , { name = 'clock'
+    , cmd  = 11
+    , get  = false
+    , docs = 'clock'
+    , args = { 'channel', u8 }
+    }
+  }
+, getters =
+  { { name = 'cv'
+    , cmd  = 8 + get_offset
+    , args = { 'channel', u8 }
+    , retval = { 'volts', u16 }
+    }
+  }
+}
+end
