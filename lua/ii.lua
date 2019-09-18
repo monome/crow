78b10a5c4920e97d18d42987102dd7e8bbe8da2c
brian crabtree <tehn@monome.org>
2019-09-18 18:21:16 -0400

    add quotes (#177)
    * add quotes
    
    * comment

--- lua/ii.lua
+++ lua/ii.lua
@@ -40,7 +40,8 @@ function ii_LeadRx_handler( addr, cmd, data )
     ii[name].event(ii[name].e[cmd], data)
 end
 
-function ii.e( name, event, data ) _c.tell('ii.'..name,tostring(event),data) end
+-- NOTE: weird double-escaped quotes down here for the c compiler
+function ii.e( name, event, data ) _c.tell('ii.'..name,'\\''..tostring(event)..'\\'',data) end
 
 ii._c =
     { cmds = { [1]='output'
