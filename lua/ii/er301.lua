93de31d2cc238de23fee4fb1628171ccfb60af94
nightmorph <nightmorph@gentoo.org>
2019-09-30 15:18:17 -0700

    revised support. er301 only expects to receive signed 16bit integers as values, and does not have GET functionality at all. it never transmits; only receives.

--- lua/ii/er301.lua
+++ lua/ii/er301.lua
@@ -6,41 +6,43 @@ do return
 , commands     =
   { { name = 'tr'
     , cmd  = 1
-    , get  = true
+    , get  = false
     , docs = 'Set TR *port* to *value* (0/1)'
     , args = { { 'port', u8 }
-             , { 'value', u8 }
+             , { 'value', s16 }
              }
     }
   , { name = 'tr_tog'
     , cmd  = 2
+    , get  = false
     , docs = 'Toggle TR *port*'
     , args = { 'port', u8 }
     }
   , { name = 'tr_pulse'
     , cmd  = 3
+    , get  = false
     , docs = 'Pulse TR *port* using TO.TR.TIME/S/M as an interval'
     , args = { 'port', u8 }
     }
   , { name = 'tr_time'
     , cmd  = 4
-    , get  = true
+    , get  = false
     , docs = 'Time for TR.PULSE *port* in *value* milliseconds'
     , args = { { 'port', u8 }
-             , { 'value', u16 }
+             , { 'value', s16 }
              }
     }
   , { name = 'tr_pol'
     , cmd  = 5
-    , get  = true
+    , get  = false
     , docs = 'Polarity for TO.TR.PULSE *port* set to *value* (0/1)'
     , args = { { 'port', u8 }
-             , { 'value', u8 }
+             , { 'value', s16 }
              }
     }
   , { name = 'cv'
     , cmd  = 6
-    , get  = true
+    , get  = false
     , docs = 'Set *port* CV to *value* (bipolar), following SLEW time'
     , args = { { 'port', u8 }
              , { 'value', s16 }
@@ -48,15 +50,15 @@ do return
     }
   , { name = 'cv_slew'
     , cmd  = 7
-    , get  = true
+    , get  = false
     , docs = 'CV *port* slew time in *value* milliseconds'
     , args = { { 'port', u8 }
-             , { 'value', u16 }
+             , { 'value', s16 }
              }
     }
   , { name = 'cv_set'
     , cmd  = 8
-    , get  = true
+    , get  = false
     , docs = 'Set CV *port* to *value* (bipolar), ignoring SLEW time'
     , args = { { 'port', u8 }
              , { 'value', s16 }
@@ -64,10 +66,10 @@ do return
     }
   , { name = 'cv_off'
     , cmd  = 9
-    , get  = true
+    , get  = false
     , docs = 'CV *port* offset, *value* added at final stage'
     , args = { { 'port', u8 }
-             , { 'value', u16 }
+             , { 'value', s16 }
              }
 }
 end
