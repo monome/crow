20e026854042cd37c8a046a8cf180973c17fa8e2
nightmorph <nightmorph@gentoo.org>
2019-10-04 11:28:50 -0700

    fixes as requested. removed redundant get=false; add missing curly braces; use s16v type for voltage ops

--- lua/ii/er301.lua
+++ lua/ii/er301.lua
@@ -6,7 +6,6 @@ do return
 , commands     =
   { { name = 'tr'
     , cmd  = 1
-    , get  = false
     , docs = 'Set TR *port* to *value* (0/1)'
     , args = { { 'port', u8 }
              , { 'value', s16 }
@@ -14,19 +13,16 @@ do return
     }
   , { name = 'tr_tog'
     , cmd  = 2
-    , get  = false
     , docs = 'Toggle TR *port*'
     , args = { 'port', u8 }
     }
   , { name = 'tr_pulse'
     , cmd  = 3
-    , get  = false
     , docs = 'Pulse TR *port* using TO.TR.TIME/S/M as an interval'
     , args = { 'port', u8 }
     }
   , { name = 'tr_time'
     , cmd  = 4
-    , get  = false
     , docs = 'Time for TR.PULSE *port* in *value* milliseconds'
     , args = { { 'port', u8 }
              , { 'value', s16 }
@@ -34,7 +30,6 @@ do return
     }
   , { name = 'tr_pol'
     , cmd  = 5
-    , get  = false
     , docs = 'Polarity for TO.TR.PULSE *port* set to *value* (0/1)'
     , args = { { 'port', u8 }
              , { 'value', s16 }
@@ -42,15 +37,13 @@ do return
     }
   , { name = 'cv'
     , cmd  = 6
-    , get  = false
     , docs = 'Set *port* CV to *value* (bipolar), following SLEW time'
     , args = { { 'port', u8 }
-             , { 'value', s16 }
+             , { 'value', s16v }
              }
     }
   , { name = 'cv_slew'
     , cmd  = 7
-    , get  = false
     , docs = 'CV *port* slew time in *value* milliseconds'
     , args = { { 'port', u8 }
              , { 'value', s16 }
@@ -58,18 +51,18 @@ do return
     }
   , { name = 'cv_set'
     , cmd  = 8
-    , get  = false
     , docs = 'Set CV *port* to *value* (bipolar), ignoring SLEW time'
     , args = { { 'port', u8 }
-             , { 'value', s16 }
+             , { 'value', s16v }
              }
     }
   , { name = 'cv_off'
     , cmd  = 9
-    , get  = false
     , docs = 'CV *port* offset, *value* added at final stage'
     , args = { { 'port', u8 }
-             , { 'value', s16 }
+             , { 'value', s16v }
              }
+    }
+  }
 }
 end
