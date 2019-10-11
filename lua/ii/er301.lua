3c93a182beb684fede905a45ca8e34697b075e24
Trent <trent.gill@gmail.com>
2019-10-11 16:47:56 -0400

    update cmd values for er301

--- lua/ii/er301.lua
+++ lua/ii/er301.lua
@@ -5,59 +5,59 @@ do return
 , lua_name     = 'er301' -- NB: must match the file name. er301.lua -> 'er301'
 , commands     =
   { { name = 'tr'
-    , cmd  = 1
+    , cmd  = 0x0
     , docs = 'Set TR *port* to *value* (0/1)'
     , args = { { 'port', u8 }
              , { 'value', s16 }
              }
     }
   , { name = 'tr_tog'
-    , cmd  = 2
+    , cmd  = 0x1
     , docs = 'Toggle TR *port*'
     , args = { 'port', u8 }
     }
   , { name = 'tr_pulse'
-    , cmd  = 3
+    , cmd  = 0x5
     , docs = 'Pulse TR *port* using TO.TR.TIME/S/M as an interval'
     , args = { 'port', u8 }
     }
   , { name = 'tr_time'
-    , cmd  = 4
+    , cmd  = 0x2
     , docs = 'Time for TR.PULSE *port* in *value* milliseconds'
     , args = { { 'port', u8 }
              , { 'value', s16 }
              }
     }
   , { name = 'tr_pol'
-    , cmd  = 5
+    , cmd  = 0x6
     , docs = 'Polarity for TO.TR.PULSE *port* set to *value* (0/1)'
     , args = { { 'port', u8 }
              , { 'value', s16 }
              }
     }
   , { name = 'cv'
-    , cmd  = 6
+    , cmd  = 0x10
     , docs = 'Set *port* CV to *value* (bipolar), following SLEW time'
     , args = { { 'port', u8 }
              , { 'value', s16V }
              }
     }
   , { name = 'cv_slew'
-    , cmd  = 7
+    , cmd  = 0x12
     , docs = 'CV *port* slew time in *value* milliseconds'
     , args = { { 'port', u8 }
              , { 'value', s16 }
              }
     }
   , { name = 'cv_set'
-    , cmd  = 8
+    , cmd  = 0x11
     , docs = 'Set CV *port* to *value* (bipolar), ignoring SLEW time'
     , args = { { 'port', u8 }
              , { 'value', s16V }
              }
     }
   , { name = 'cv_off'
-    , cmd  = 9
+    , cmd  = 0x15
     , docs = 'CV *port* offset, *value* added at final stage'
     , args = { { 'port', u8 }
              , { 'value', s16V }
