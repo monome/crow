d05791b47ca58b7c18cd13cdf8cddf07dddb4c58
sw <wolk.sam@gmail.com>
2019-10-20 15:54:15 -0400

    typo fix from merge

--- lua/asllib.lua
+++ lua/asllib.lua
@@ -50,7 +50,7 @@ end
 
 function lfo( time, level )
     time, level = time or 1, level or 5
-    local function half(t) = m1( function(a) return clamp(a/2) end, t )
+    local function half(t) = m1( function(a) return clamp(a/2) end, t ) end
 
     return loop{ to(        level , half(time) )
                , to( negate(level), half(time) )}
