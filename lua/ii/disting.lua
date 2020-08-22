00aaabfce8807ed7a8dba133d83100dbb4c923c9
Carl Colglazier <carl@carlcolglazier.com>
2020-08-22 10:39:30 -0400

    Use u8 types for algorithms.
    Add docs to most commands.
    Add missing commands from manual.

--- lua/ii/disting.lua
+++ lua/ii/disting.lua
@@ -6,10 +6,12 @@ do return
 , commands     =
   { { name = 'load_preset'
     , cmd = 0x40
-    , args = { 'value', u16 }
+    , docs = 'load a preset'
+    , args = { 'number', u16 }
     }
   , { name = 'save_preset'
     , cmd = 0x41
+    , docs = 'save as a preset'
     , args = { 'value', u16 }
     }
   , { name = 'reset_preset'
@@ -17,59 +19,104 @@ do return
     }
   , { name = 'load_algorithm'
     , cmd = 0x44
-    , args = { 'algorithm', u16 }
+    , docs = 'load single mode algorithm'
+    , args = { 'algorithm', u8 }
     }
   , { name = 'set_controller'
     , cmd = 0x11
+    , docs = 'sets parameters via mappings'
     , args = { { 'controller', u8 }
              , { 'value', u16 }
              }
     }
   , { name = 'set_parameter'
     , cmd = 0x46
-    , args = { { 'parameter', u16 }
+    , args = { { 'parameter', u8 }
              , { 'value', u16 }
              }
     }
+  , { name = 'set_scale_parameter'
+    , cmd = 0x47
+    , docs = 'The 0-16384 value range scales to the actual param. value range'
+    , args = { { 'parameter', u8 }
+             , { 'value', u16 }
+             }
+    }
+  -- these are algorithm-specific
   , { name = 'wav_record'
     , cmd = 0x4B
-    -- TODO: args
+    , docs = '0 for stop; 1 for start'
+    , args = { 'action', u8 }
     }
   , { name = 'wav_playback'
     , cmd = 0x4C
-    -- TODO: args
+    , args = { 'action', u8 }
     }
   , { name = 'al_pitch'
     , cmd = 0x4D
     , args = { 'pitch', u16 }
+    , docs = 'Set Augustus Loop pitch'
     }
   , { name = 'al_clock'
     , cmd = 0x4E
+    , docs = 'Send Augustus Loop clock'
     }
-  -- TODO: MIDI / select bus
+  --MIDI / select bus
   , { name = 'midi'
     , cmd = 0x4F
     , args = { { 'status', u8 }
+             , { 'b0', u8 }
              , { 'b1', u8 }
-	     , { 'b2', u8 }
-	     }
+             }
+    }
+  , { name = 'select_bus'
+    , cmd = 0x50
+    , args = { { 'status', u8 }
+             , { 'b0', u8 }
+             , { 'b1', u8 }
+             }
     }
   -- voice control
   , { name = 'voice_pitch'
     , cmd = 0x51
+    , docs = 'Set voice pitch for the specified voice'
     , args = { { 'voice', u8 }
              , { 'pitch', s16 }
-	     }
+             }
     }
-  , { name = 'note_on'
+  , { name = 'voice_on'
     , cmd = 0x52
+    , docs = 'note on for the specified voice'
     , args = { { 'voice', u8 }
              , { 'velocity', s16 }
              }
     }
-  , { name = 'note_off'
+  , { name = 'voice_off'
     , cmd = 0x53
+    , docs = 'note off for the specified voice'
     , args = { 'voice', u8 }
+    }
+  , { name = 'note_pitch'
+    , cmd = 0x54
+    , docs = 'set voice pitch for note id'
+    , args = { { 'note_id', u8 }
+             , { 'pitch', s16 }
+             }
+    }
+  , { name = 'note_pitch'
+    , cmd = 0x55
+    , docs = 'set voice velocity for note id'
+    , args = { { 'note_id', u8 }
+             ,  { 'velocity', s16 }
+             }
+    }
+  , { name = 'note_off'
+    , cmd = 0x56
+    , docs = 'note off for note id'
+    , args = { 'note_id', u8 }
+    }
+  , { name = 'all_notes_off'
+    , cmd = 0x57
     }
   }
 , getters =
