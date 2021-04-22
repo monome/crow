883608fd9c7f35c9a4855aa2324723670da0254b
= <=>
2021-04-21 18:13:37 -0700

    update to new usb device library

--- usbd/usbd_conf.h
+++ usbd/usbd_conf.h
@@ -61,7 +61,6 @@
 #define USBD_MAX_NUM_INTERFACES               1
 #define USBD_MAX_NUM_CONFIGURATION            1
 #define USBD_MAX_STR_DESC_SIZ                 0x100
-#define USBD_SUPPORT_USER_STRING              0 
 #define USBD_SELF_POWERED                     1
 #define USBD_DEBUG_LEVEL                      0
 
@@ -73,29 +72,35 @@ void* malloc1( size_t size ); // like calloc, but sets to ones (??!?!?!)
 #define USBD_memset               memset
 #define USBD_memcpy               memcpy
     
-/* DEBUG macros */  
-#if (USBD_DEBUG_LEVEL > 0)
-#define  USBD_UsrLog(...)   printf(__VA_ARGS__);\
-                            printf("\n");
+/* DEBUG macros */
+#if (USBD_DEBUG_LEVEL > 0U)
+#define  USBD_UsrLog(...)   do { \
+                            printf(__VA_ARGS__); \
+                            printf("\n"); \
+} while (0)
 #else
-#define USBD_UsrLog(...)   
-#endif                            
-                            
-#if (USBD_DEBUG_LEVEL > 1)
+#define USBD_UsrLog(...) do {} while (0)
+#endif
 
-#define  USBD_ErrLog(...)   printf("ERROR: ") ;\
-                            printf(__VA_ARGS__);\
-                            printf("\n");
+#if (USBD_DEBUG_LEVEL > 1U)
+
+#define  USBD_ErrLog(...) do { \
+                            printf("ERROR: ") ; \
+                            printf(__VA_ARGS__); \
+                            printf("\n"); \
+} while (0)
 #else
-#define USBD_ErrLog(...)   
-#endif 
-                                                        
-#if (USBD_DEBUG_LEVEL > 2)                         
-#define  USBD_DbgLog(...)   printf("DEBUG : ") ;\
-                            printf(__VA_ARGS__);\
-                            printf("\n");
+#define USBD_ErrLog(...) do {} while (0)
+#endif
+
+#if (USBD_DEBUG_LEVEL > 2U)
+#define  USBD_DbgLog(...)   do { \
+                            printf("DEBUG : ") ; \
+                            printf(__VA_ARGS__); \
+                            printf("\n"); \
+} while (0)
 #else
-#define USBD_DbgLog(...)                         
+#define USBD_DbgLog(...) do {} while (0)
 #endif
 
 /* Exported functions ------------------------------------------------------- */
