0565318be6ffb4d7c413d4d1a7a4da315eb08778
= <=>
2021-06-03 05:59:43 -0700

    cleanup RNG LL file

--- ll/random.c
+++ ll/random.c
@@ -3,20 +3,18 @@
 #include <stm32f7xx.h>
 #include <stdio.h>
 
-RNG_HandleTypeDef r;
+static RNG_HandleTypeDef r;
 
-#define rrrMAX 16
-float rrrFm[rrrMAX];
-int8_t rrrNext = 0;
-int8_t rrrCount = 0;
+#define BUFLEN 16
+static float buf[BUFLEN];
+static int8_t head = 0;
+static int8_t count = 0;
 
 void Random_Init(void)
 {
-    // config r
     r.Instance = RNG;
     HAL_RNG_Init(&r);
 
-    // TODO need a HAL_Delay(1) here?
     if( HAL_RNG_GenerateRandomNumber_IT(&r) ){
         printf("rng failed to start\n");
     }
@@ -30,7 +28,6 @@ void Random_DeInit(void)
 void HAL_RNG_MspInit(RNG_HandleTypeDef *hrng)
 {
     __RNG_CLK_ENABLE(); // __HAL_RCC_RNG_CLK_ENABLE()
-
     HAL_NVIC_SetPriority( RNG_IRQn
                         , RNG_IRQPriority
                         , RNG_IRQSubPriority);
@@ -43,7 +40,7 @@ void HAL_RNG_MspDeInit(RNG_HandleTypeDef *hrng)
     __RNG_RELEASE_RESET();
 }
 
-//FIXME what is this IRQHandler?
+// Forward LL callback to HAL
 void RNG_IRQHandler( void )
 {
     HAL_RNG_IRQHandler(&r);
@@ -57,19 +54,19 @@ void HAL_RNG_ErrorCallback( RNG_HandleTypeDef* hr )
 
 void HAL_RNG_ReadyDataCallback( RNG_HandleTypeDef* hr, uint32_t rand32 )
 {
-    if( rrrCount < rrrMAX ){ // make sure we have room to store the val
-        int8_t ix = (rrrNext + rrrCount++) % rrrMAX;
-        rrrFm[ix] = (float)rand32 / (1.0 + (float)(uint32_t)0xFFFFFFFF);
+    if( count < BUFLEN ){ // make sure we have room to store the val
+        int8_t ix = (head + count++) % BUFLEN;
+        buf[ix] = (float)rand32 / (1.0 + (float)(uint32_t)0xFFFFFFFF);
         Random_Update();
     }
 }
 
 float Random_Float(void)
 {
-    if( rrrCount <= 0 ){ printf("rng: underrun\n"); }
-    float retval = rrrFm[rrrNext++];
-    rrrNext %= rrrMAX;
-    if( --rrrCount < (rrrMAX/4) ){ // update if 3/4 empty
+    if( count <= 0 ){ printf("rng: underrun\n"); }
+    float retval = buf[head++];
+    head %= BUFLEN;
+    if( --count < (BUFLEN/4) ){ // update if 3/4 empty
         Random_Update();
     }
     return retval;
@@ -77,12 +74,12 @@ float Random_Float(void)
 
 int Random_Int(int lower, int upper)
 {
-    return ((int)(Random_Float() * (float)(1 + upper - lower)) + lower);
+    return (int)(Random_Float() * (float)(1 + upper - lower)) + lower;
 }
 
 void Random_Update(void)
 {
-    if( rrrCount != rrrMAX ){
+    if( count != BUFLEN ){
         if( HAL_RNG_GenerateRandomNumber_IT(&r) ){
             printf("rng failed to fill\n");
         }
