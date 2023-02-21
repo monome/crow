0315ea0dfda3dfd1b1eb72e91a66c7153e2f199b
trent <trent.gill@gmail.com>
2023-02-20 18:21:47 -0800

    Sequins transformers (#449)
    * sequins `map` transformers working with function modifiers. tests included
    
    * add arithmetic operator support for simple transformers
    
    * :reset() method correctly resets all flow modifiers & the map
    
    * :setdata() now works across all elements as well

--- lua/sequins.lua
+++ lua/sequins.lua
@@ -21,7 +21,7 @@ function S.new(t)
               , qix    = 1 -- force 1st value to 1st step
               , n      = 1 -- store the step val (can be a sequins)
               , flw    = {} -- store any applied flow modifiers
-              , fn     = {} -- store a transformer function & any additional arguments
+              , fun    = {} -- store a transformer function & any additional arguments
               }
     return setmetatable(s, S)
 end
@@ -32,15 +32,48 @@ function S:is_sequins() return getmetatable(self) == S end
 
 function S:setdata(t)
     if S.is_sequins(t) then
-        t = t.data -- handle sequins as input
+        do -- flow modifiers
+            local src = t.flw
+            local dst = self.flw
 
-        -- FIXME generalize to cover flow-mods & transforms
-        -- only need to worry about it in here
-        -- otherwise it's just a raw table (hence untouched)
+            -- update / remove existing
+            for k,v in pairs(dst) do
+                if src[k] then -- already exists
+                    dst[k].n  = src[k].n -- update value
+                else dst[k] = nil end -- removed
+            end
+
+            -- add any new
+            for k,v in pairs(src) do
+                if not dst[k] then -- newly added
+                    dst[k] = {ix = v.ix, n = v.n} -- manual copy
+                end
+            end
+        end
+
+        do -- update data of transformers
+            local src = t.fun
+            local dst = self.fun
+
+            if dst[1] then -- already have a transformer
+                dst[1] = src[1] -- copy function
+                for k,v in ipairs(src[2]) do
+                     if S.is_sequins(v) and S.is_sequins(dst[2][k]) then
+                        dst[2][k]:settable(v) -- recurse nested sequins
+                    else
+                        dst[2][k] = v -- copy piecemeal
+                    end
+                end
+            end
+        end
+
+        -- finally, place data over top of input sequins
+        t = t.data -- handle sequins data as input table
     end
 
     t = totable(t) -- convert a string to a table of chars
 
+    -- data swap
     for i=1,#t do
         if S.is_sequins(t[i]) and S.is_sequins(self.data[i]) then
             self.data[i]:settable(t[i]) -- recurse nested sequins
@@ -54,30 +87,26 @@ function S:setdata(t)
     self.ix = wrap_index(self, self.ix)
 end
 
--- nb: 2nd arg 'cp' is for internal recursive use only
-function S.copy(og, cp)
-    cp = cp or {}
-    local og_type = type(og)
-    local copy = {}
-    if og_type == 'table' then
-        if cp[og] then -- handle duplicate refs to an internal table
-            copy = cp[og]
-        else
-            cp[og] = copy
-            for og_k, og_v in next, og, nil do
-                copy[S.copy(og_k, cp)] = S.copy(og_v, cp)
-            end
-            setmetatable(copy, S.copy(getmetatable(og), cp))
+function S:copy()
+    if type(self) == 'table' then
+        local cp = {}
+        for k,v in pairs(self) do
+            cp[k] = S.copy(v)
         end
-    else copy = og end -- literal value
-    return copy
+        return setmetatable(cp, getmetatable(self))
+    else return self end
 end
 
 function S:peek() return self.data[self.ix] end
 
-
-------------------------------
---- control flow execution
+function S:bake(n)
+    n = n or #self -- void call creates a table of length == the parent sequins
+    local t = {}
+    for i=1,n do
+        t[i] = self()
+    end
+    return S.new(t)
+end
 
 local function turtle(t, fn)
     -- apply fn to all nested sequins
@@ -85,6 +114,41 @@ local function turtle(t, fn)
     if S.is_sequins(t) then return fn(t) end -- unwrap
     return t -- literal value
 end
+
+------------------------------
+--- transformers
+
+function S:func(fn, ...)
+    self.fun = {fn, {...}} -- capture function & any args (sequins)
+    return self
+end
+
+local function do_transform(s, v)
+    if s.fun[1] then -- implicitly handles a cleared function
+        if #s.fun[2] > 0 then
+            return s.fun[1](v, table.unpack(s.fun[2]))
+        else return s.fun[1](v) end
+    else return v end
+end
+
+-- support arithmetic via math operators
+S._fns = {
+    add = function(n,b) return n+turtle(b) end,
+    sub = function(n,b) return n-turtle(b) end,
+    mul = function(n,b) return n*turtle(b) end,
+    div = function(n,b) return n/turtle(b) end,
+    mod = function(n,b) return n%turtle(b) end,
+}
+
+-- assume 'a' is self of a sequins
+S.__add = function(a,b) return S.func(a, S._fns.add, b) end
+S.__sub = function(a,b) return S.func(a, S._fns.sub, b) end
+S.__mul = function(a,b) return S.func(a, S._fns.mul, b) end
+S.__div = function(a,b) return S.func(a, S._fns.div, b) end
+S.__mod = function(a,b) return S.func(a, S._fns.mod, b) end
+
+------------------------------
+--- control flow execution
 
 local function do_step(s)
     -- if .qix is set, it will be used, rather than incrementing by s.n
@@ -107,8 +171,8 @@ S.flows = {
     end
 }
 
-function do_flow(s, k)
-    local f = s.flw[k] -- check if times exists
+local function do_flow(s, k)
+    local f = s.flw[k] -- check if flow-mod exists
     if f then
         f.ix = f.ix + 1
         return S.flows[k](f, turtle(f.n))
@@ -123,7 +187,7 @@ function S.next(s)
         local e = s.flw.every
         if e then e.ix = e.ix - 1 end -- undo every advance
     end
-    return do_step(s), again
+    return do_transform(s,do_step(s)), again
 end
 
 function S:step(n) self.n = n; return self end
@@ -145,6 +209,11 @@ function S:reset()
     for _,v in pairs(self.flw) do
         v.ix = 0
         turtle(v.n, S.reset)
+    end
+    if self.fun[1] and #self.fun[2] > 0 then
+        for _,v in pairs(self.fun[2]) do
+            turtle(v, S.reset)
+        end
     end
 end
 
@@ -169,6 +238,8 @@ S.metaix = { settable = S.setdata
            , select   = S.select
            , peek     = S.peek
            , copy     = S.copy
+           , map      = S.func
+           , bake     = S.bake
            }
 S.__index = function(self, ix)
     if type(ix) == 'number' then return self.data[ix]
@@ -194,12 +265,16 @@ S.__tostring = function(t)
     -- modifiers
     for k,v in pairs(t.flw) do
         -- TODO do we need to print current counters?
-        s = string.format('%s:%s(%s)',s, k:sub(1,1), tostring(v.n))
+        s = string.format('%s:%s[%i](%s)',s, k:sub(1,1), v.ix, tostring(v.n))
     end
 
     -- transformer
-    if #t.fn > 0 then
-        s = string.format('%s:fn(%s)',s, k:sub(1,1), tostring(t.fn[1]))
+    if #t.fun > 0 then
+        local fns = {'fn'}
+        for i=1,#t.fun[2] do
+            fns[i+1] = tostring(t.fun[2][i])
+        end
+        s = string.format('%s:map(%s)',s, table.concat(fns, ','))
     end
 
     return s
